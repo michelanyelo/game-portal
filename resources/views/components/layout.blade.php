4d5afa3693c8454a0d29c48f32cdf9aabc4541c8
michelanyelo.dev <miguelmonzon.inf@gmail.com>
2025-01-28 21:56:21 -0300

    Agregar barra de búsqueda y pie de página al diseño

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -28,10 +28,40 @@
                     <li><a href="#" class="hover:text-gray-400">Cooming soon</a></li>
                 </ul>
             </div>
+
+            <div class="flex items-center">
+                <div class="relative">
+                    <input type="text"
+                        class="bg-gray-800 text-sm rounded-full focus:outline-none focus:shadow-outline w-64 px-3 py-1 pl-8"
+                        placeholder="Buscar...">
+                    <div class="absolute top-0 flex items-center h-full ml-2">
+                        <svg class="w-4 h-4 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-5-5">
+                            </path>
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M15 10a5 5 0 1 1-10 0 5 5 0 0 1 10 0z"></path>
+                        </svg>
+                    </div>
+                </div>
+                <div class="ml-6">
+                    <a href="#">
+                        <img src="https://assets.laracasts.com/images/mascot/larywink.svg" alt="avatar"
+                            class="rounded-full w-8">
+                    </a>
+                </div>
+            </div>
         </nav>
     </header>
 
-    {{ $slot }}
+    <main class="py-8">
+        {{ $slot }}
+    </main>
+
+    <footer class="bg-gray-800 text-gray-400 py-8">
+        <div class="container mx-auto px-4 py-6">
+            Hecho por <a href="#" class="underline hover:text-gray-300">michelanyelo.dev</a>
+        </div>
+    </footer>
 </body>
 
 </html>
