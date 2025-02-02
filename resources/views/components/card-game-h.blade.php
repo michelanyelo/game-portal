48ec39c11476173cb436c20958df5b3aaf70d9b0
michelanyelo.dev <miguelmonzon.inf@gmail.com>
2025-02-02 13:34:24 -0300

    Refactor welcome view to use component-based structure for popular and recently reviewed games

---
+++ resources/views/components/card-game-h.blade.php
@@ -0,0 +1,29 @@
+<div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
+    <div class="relative flex-none">
+        <a href="#">
+            <img src="https://upload.wikimedia.org/wikipedia/en/c/ce/FFVIIRemake.png"
+                 alt="Final Fantasy VII Remake"
+                 class="w-20 lg:w-48 hover:opacity-75 transition ease-in-out duration-150">
+        </a>
+
+        <div class="absolute bottom-0 right-0 w-12 h-12 bg-gray-900 rounded-full"
+             style="right: -20px; bottom: -20px;">
+            <div class="font-semibold text-xs flex justify-center items-center h-full">
+                80%
+            </div>
+        </div>
+    </div>
+
+    <div class="ml-6 lg:ml-12">
+        <a href="#"
+           class="block text-lg font-semibold leading-tight hover:text-gray-400 mt-4">
+            Final Fantasy VII Remake
+        </a>
+        <div class="text-gray-400 mt-1">Playstation 4</div>
+        <p class="mt-6 text-gray-400 hidden lg:block">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto eius expedita quod
+            sequi quis ad suscipit labore ullam repudiandae modi error, deleniti voluptatum illo
+            temporibus, facere eligendi non dolore corrupti?
+        </p>
+    </div>
+</div>
---
+++ resources/views/components/most-anticipated.blade.php
@@ -0,0 +1,13 @@
+<div class="game flex">
+    <a href="#">
+        <img
+            src="https://image.api.playstation.com/vulcan/ap/rnd/202008/0416/6Bo40lnWU0BhgrOUm7Cb6by3.png"
+            alt="Cyberpunk 2077"
+            class="w-16 hover:opacity-75 transition ease-in-out duration-150">
+    </a>
+
+    <div class="ml-4">
+        <a href="#" class="hover:text-gray-300">Cyberpunk 2077</a>
+        <div class="text-gray-400">Septiembre 16, 2020</div>
+    </div>
+</div>
